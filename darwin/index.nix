3cd91b4fb0055a3936269632ff7c7501044e7874
Ven Vaneless <12345678+venvaneless@users.noreply.github.com>
2025-11-20 07:37:43 +0100

    Installing mkcert and nssTools 20251120-0737

--- darwin/index.nix
+++ darwin/index.nix
@@ -25,8 +25,8 @@
     # --- docker ---
     ./modules/services/docker.nix
     ./modules/services/vaultwarden.nix
-    # ./modules/services/vaultwarden-nginx-test.nix
-    ./modules/services/vaultwarden-nginx.nix
+    ./modules/services/vaultwarden-nginx-test.nix
+    # ./modules/services/vaultwarden-nginx.nix
 
    	# --- home-manager last ---
     ../shared/services/home-manager.nix
