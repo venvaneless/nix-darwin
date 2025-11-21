46e0c75f986c698a161dab9b09a4f398a524e3b6
Ven Vaneless <12345678+venvaneless@users.noreply.github.com>
2025-11-21 10:13:30 +0100

    Add an alias staging and commiting git changes

--- darwin/modules/terminal/zsh.nix
+++ darwin/modules/terminal/zsh.nix
@@ -61,6 +61,7 @@
       drh  = "home-manager switch --flake /Users/ven/dotfiles/nix#ven";
       drhb = "home-manager build --flake /Users/ven/dotfiles/nix#ven";
       drg  = "sudo -H nix-env --list-generations --profile /nix/var/nix/profiles/system";
+      gsn = "/Users/ven/iCloudDocs/my-system/00-sys_assets/scripts/git-stage.sh";
     };
   };
 
