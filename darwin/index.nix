2e2631245bbf577892213105996a61d721a07af8
Ven Vaneless <12345678+venvaneless@users.noreply.github.com>
2025-11-21 05:49:04 +0100

    Remove old vaultwarden-nginx-test.nix

--- darwin/index.nix
+++ darwin/index.nix
@@ -25,7 +25,6 @@
     # --- docker ---
     ./modules/services/docker.nix
     ./modules/services/vaultwarden.nix
-    # ./modules/services/vaultwarden-nginx2.nix
     ./modules/services/vaultwarden-nginx.nix
 
    	# --- home-manager last ---
