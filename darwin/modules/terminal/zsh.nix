69cd8731fde5f1248db35129e981b879556d82e5
Ven Vaneless <12345678+venvaneless@users.noreply.github.com>
2025-11-21 13:08:06 +0100

    Add comment for test scripts in zsh.nix

--- darwin/modules/terminal/zsh.nix
+++ darwin/modules/terminal/zsh.nix
@@ -64,6 +64,7 @@
       gsn = "/Users/ven/iCloudDocs/my-system/00-sys_assets/scripts/git-scripts/nix-repo.sh";
       gsd = "/Users/ven/iCloudDocs/my-system/00-sys_assets/scripts/git-scripts/dotfiles-repo.sh";
       
+      # TEST SCRIPTS
       gsnt = "/Users/ven/iCloudDocs/my-system/00-sys_assets/scripts/git-scripts/nix-repo-test.sh";
       gsdt = "/Users/ven/iCloudDocs/my-system/00-sys_assets/scripts/git-scripts/dotfiles-repo-test.sh";
     };
