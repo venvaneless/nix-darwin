5ee5c5ebb5530930081429871b6586d232afb945
Ven <209770139+venvaneless@users.noreply.github.com>
2025-11-24 07:04:21 +0100

    - Change paths for 'symlinking.nix' and 'zsh.nix' inside both 'home-manager.nix' and 'home-manager-standalone.nix'

--- darwin/modules/system/home-manager-standalone.nix
+++ darwin/modules/system/home-manager-standalone.nix
@@ -30,7 +30,7 @@
 
   # --- User modules ---
   imports = [
-    ../../terminal/zsh.nix
-    ../../apps/user-data/symlinking.nix
+    ../terminal/zsh.nix
+    ../apps/user-data/symlinking.nix
   ];
 }
