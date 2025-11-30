7d64c0ea9f097255b2d4de2666ccb6f285c1e991
Ven <209770139+venvaneless@users.noreply.github.com>
2025-11-30 16:17:16 +0100

    Fix cachix public key error

--- darwin/modules/system/base.nix
+++ darwin/modules/system/base.nix
@@ -20,12 +20,10 @@
 
     # Use the Nix Community binary cache
     substituters = [
-      "https://nix-community.cachix.org"
       "https://cache.nixos.org"
     ];
 
     trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
