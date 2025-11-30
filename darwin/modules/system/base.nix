a698558893623b1c498ce8b112d2e79064324343
Ven <209770139+venvaneless@users.noreply.github.com>
2025-11-30 16:00:06 +0100

    Fix cachix public key error

--- darwin/modules/system/base.nix
+++ darwin/modules/system/base.nix
@@ -1,34 +1,44 @@
- # /Users/ven/dotfiles/nix/darwin/modules/system/base.nix
- #
- # CORE CONFIG
-  # ============================================================
-  # BASE CONFIG
-  # Enable flakes, small services, expose additional flags
-  # and commands for the "nix-darwin" etc.
-  # ============================================================
+# /Users/ven/dotfiles/nix/darwin/modules/system/base.nix
+#
+# CORE CONFIG
+# ============================================================
+# BASE CONFIG
+# Enable flakes, small services, expose additional flags
+# and commands for the "nix-darwin" etc.
+# ============================================================
 
-  { config, pkgs, lib, inputs, ... }:
-  
-  {
-    # --- Core system identity ---
-    networking.hostName = "Vens-MacBook-Pro";
+{ config, pkgs, lib, inputs, ... }:
 
-    # --- Enable new Nix CLI and flakes ---
-    nix.settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      build-users-group = "nixbld";
-    };
+{
+  # --- Core system identity ---
+  networking.hostName = "Vens-MacBook-Pro";
 
-    # --- Enable Nix Determine ---
-    nix.enable = false;
+  # --- Enable new Nix CLI and flakes ---
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    build-users-group = "nixbld";
 
+    # Use the Nix Community binary cache
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org"
+    ];
 
-    # --- System state version (Darwin revision) ---
-    system.stateVersion = 6;
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
 
-    # --- Global system packages ---
-    environment.systemPackages = with pkgs; [
-    inputs.darwin.packages.${pkgs.system}.darwin-rebuild
+  # --- Enable Nix Determine ---
+  nix.enable = false;
+
+  # --- System state version (Darwin revision) ---
+  system.stateVersion = 6;
+
+  # --- Global system packages ---
+  environment.systemPackages = with pkgs; [
+    inputs.darwin.packages.${pkgs.stdenv.hostPlatform.system}.darwin-rebuild
     bashInteractive
     git-crypt
     nh
@@ -38,5 +48,5 @@
     nginx
     nixd
     nil
-    ];
-  }
+  ];
+}
