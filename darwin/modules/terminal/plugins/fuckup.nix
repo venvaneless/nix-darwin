f4852d804ab3cec4a9688c8653c0cbe8f2ca07f3
Ven <209770139+venvaneless@users.noreply.github.com>
2025-12-07 21:09:27 +0100

    Enable fuckup

--- darwin/modules/terminal/plugins/fuckup.nix
+++ darwin/modules/terminal/plugins/fuckup.nix
@@ -3,7 +3,7 @@
 {
   home.packages = [ pkgs.fuckup ];
 
-  programs.zsh.initExtraConfig = ''
+  programs.zsh.initExtra = ''
     # FUCKUP: fix last command (thefuck replacement)
     eval "$(fuckup --alias)"
   '';
