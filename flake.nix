3c3c53788b335d2414446f0c77ef47adfd2cdd52
Ven <209770139+venvaneless@users.noreply.github.com>
2025-12-08 04:15:51 +0100

    Revert back to integrated HM

--- flake.nix
+++ flake.nix
@@ -18,7 +18,14 @@
     # nix-darwin
     darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.11";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-  
+    
+    # Unstable branch
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Unstable overlays
+    # Access to your main nixpkgs
+    nixpkgs-unstable.inputs.nixpkgs.follows = "nixpkgs";
+
     # Home Manager
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
