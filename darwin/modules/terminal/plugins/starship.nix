5f90a162a12cad891fa14597cd6032a6ded90a4a
Ven <209770139+venvaneless@users.noreply.github.com>
2025-12-10 02:56:10 +0100

    Set custom paths for Starship - Fix

--- darwin/modules/terminal/plugins/starship.nix
+++ darwin/modules/terminal/plugins/starship.nix
@@ -14,7 +14,7 @@
   programs.zsh = {
     sessionVariables = {
       STARSHIP_CONFIG = "${config.home.homeDirectory}/ven-dots/zsh/starship.toml";
-      STARSHIP_CACHE = "${config.home.homeDirectory}/ven-dots/starship/cache";
+      STARSHIP_CACHE = "${config.home.homeDirectory}/ven-dots/zsh/starship/cache";
     };
   };
 }
