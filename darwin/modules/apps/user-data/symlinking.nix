c983b13fe3c48a764fbadc7f078c8cf1821b33e0
Ven <209770139+venvaneless@users.noreply.github.com>
2025-12-16 23:44:43 +0100

    Debugging symlinking v2

--- darwin/modules/apps/user-data/symlinking.nix
+++ darwin/modules/apps/user-data/symlinking.nix
@@ -13,12 +13,12 @@
 
 {
   imports = [
-  	./abetterfinderattributes-symlinks.nix
-   	./abetterfinderrename-symlinks.nix
+  	#	./abetterfinderattributes-symlinks.nix
+   	#	./abetterfinderrename-symlinks.nix
     # ./chromium-symlinks.nix
-    # ./espanso-symlinks.nix
+    ./espanso-symlinks.nix
     # ./iterm-symlinks.nix
-    # ./yate-symlinks.nix
+    ./yate-symlinks.nix
     # ./vlc-symlinks.nix
     # ./zed-symlinks.nix
   ];
