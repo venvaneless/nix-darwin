9a051a205a65e2bcf91159d4fd6a9763ee39bb0e
Ven <209770139+venvaneless@users.noreply.github.com>
2025-12-16 02:52:45 +0100

    Debugging app symlinks v3 - Activating apps

--- darwin/modules/apps/apps.nix
+++ darwin/modules/apps/apps.nix
@@ -16,11 +16,11 @@
       ./iterm.nix
       ./mas.nix
       ./obsidian.nix
-     # ./paste.nix
+      ./paste.nix
       ./raycast.nix
-    #  ./vlc.nix
+      ./vlc.nix
       ./wezterm.nix
       ./zed.nix
-     # ./yate.nix
+      ./yate.nix
   ];
 }
