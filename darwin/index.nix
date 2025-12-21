a495bf00a1ed9a4cb1015ac59145e23f08d5dc83
Ven <209770139+venvaneless@users.noreply.github.com>
2025-12-21 08:04:23 +0100

    Debugging HM v7

--- darwin/index.nix
+++ darwin/index.nix
@@ -26,7 +26,7 @@
   system.stateVersion = lib.mkForce 6;
   
   users.users.ven.home = "/Users/ven";
-
+  home-manager.backupFileExtension = "bak";
   # ------------------------------------------------------------
   # 
   # --- MODULE IMPORTS ---
