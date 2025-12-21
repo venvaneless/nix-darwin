3d5567d01fc7804bcb10e9cc27165bec1475ca76
Ven <209770139+venvaneless@users.noreply.github.com>
2025-12-21 03:17:35 +0100

    Fixing aliases v10

--- darwin/modules/terminal/zsh.nix
+++ darwin/modules/terminal/zsh.nix
@@ -17,7 +17,7 @@
 	    enable = true;
 	
 	    # Keeps zshrc owned by HM here
-	    dotDir = "${config.home.homeDirectory}/ven-dots/zsh-new";
+	    dotDir = "${config.home.homeDirectory}/ven-dots/zsh";
 			
 			# Temporary
 	  };
