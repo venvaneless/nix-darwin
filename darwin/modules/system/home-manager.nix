e657962ea6cd4c5139aa28c1b743809236144e69
Ven <209770139+venvaneless@users.noreply.github.com>
2025-12-22 00:55:06 +0100

    Debbuging flake 2025-12-22 00:55

--- darwin/modules/system/home-manager.nix
+++ darwin/modules/system/home-manager.nix
@@ -30,6 +30,7 @@
       home.stateVersion  = "25.11";
       home.file.".zshenv".text = ''export ZDOTDIR="$HOME/ven-dots/zsh"'';
       home.file."ven-dots/zsh/.zshrc".text = "";
+      home.file."ven-dots/zsh".recursive = true;
       
       # --- Astrovim config --- #
       home.packages = [
