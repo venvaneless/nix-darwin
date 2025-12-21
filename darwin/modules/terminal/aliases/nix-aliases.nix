97ac0c4970c423141681e9004b1b82e89fd38361
Ven <209770139+venvaneless@users.noreply.github.com>
2025-12-21 02:21:16 +0100

    Fixing aliases v4

--- darwin/modules/terminal/aliases/nix-aliases.nix
+++ darwin/modules/terminal/aliases/nix-aliases.nix
@@ -42,7 +42,7 @@
 		
 		# Updates the flake after new lockfile
 		# ------------------------------------
-		ndu  = "nix flake update --flake ~/.config/nix/nix-darwin";
+		# ndu  = "nix flake update --flake ~/.config/nix/nix-darwin";
 		
 		# Temporary
 		# 🔴 PROOF ALIAS (temporary)
