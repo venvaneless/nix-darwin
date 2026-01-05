bdcde42c610d593b0a5e2507dff9eceb6bc3d1e1
Ven <209770139+venvaneless@users.noreply.github.com>
2026-01-05 10:05:40 +0100

    Switch homebrew from cleanup zap to uninstall. Remove './quarantine-fixes.nix''

--- darwin/modules/system/homebrew.nix
+++ darwin/modules/system/homebrew.nix
@@ -13,7 +13,7 @@
   # --- Loads Homebrew
   imports = [
     nix-homebrew.darwinModules.nix-homebrew
-    ./quarantine-fixes.nix
+    # ./quarantine-fixes.nix
   ];
 
   # --- Configures the nix-homebrew backend
