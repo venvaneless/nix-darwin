4da3c7446cd04dcbcbe17423e0cad8f9d33b4f64
Ven <209770139+venvaneless@users.noreply.github.com>
2026-02-22 04:05:18 +0100

    Re-add Hammerspoon

---
+++ darwin/modules/apps/hammerspoon.nix
@@ -0,0 +1,41 @@
+# /Users/ven/.config/nix/nix-darwin/darwin/modules/apps/hammerspoon.nix
+# 
+# ============================================================
+# HAMMERSPOOON
+# 
+# Hammerspoon is a tool for powerful automation of macOS.
+# At its core, Hammerspoon is just a bridge between
+# the operating system and a Lua scripting engine.
+# ============================================================
+
+{ ... }:
+
+let
+# App metadata
+# ------------------------------------------------------------
+	appName   = "Hammerspoon.app";
+	caskName  = "hammerspoon";
+  targetDir = "/Applications/System";
+in
+{
+
+# Homebrew cask install
+# ------------------------------------------------------------
+  homebrew.casks = [
+    {
+      name = caskName;
+      args = { appdir = targetDir; };
+    }
+  ];
+
+  # Ensure application directory exists
+  # ------------------------------------------------------------
+  system.activationScripts.ensureHammerspoonAppDir.text = ''
+    if [ ! -d "${targetDir}" ]; then
+      echo "[${appName}] Creating application directory: ${targetDir}"
+      mkdir -p "${targetDir}"
+    else
+      echo "[${appName}] Application directory already exists: ${targetDir}"
+    fi
+  '';
+}
