b808162e84f4e32c00cb58adf4ee7ab1dc73ff30
Ven <209770139+venvaneless@users.noreply.github.com>
2026-04-18 18:24:38 +0200

    Adding themes and plugins for yazi

--- darwin/modules/terminal/plugins/yazi.nix
+++ darwin/modules/terminal/plugins/yazi.nix
@@ -9,18 +9,11 @@
 { pkgs, lib, ... }:
 
 let
-  catppuccinYazi = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "yazi";
+  catppuccinFlavors = pkgs.fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "flavors";
     rev = "main";
     hash = "sha256-Og33IGS9pTim6LEH33CO102wpGnPomiperFbqfgrJjw=";
-  };
-
-  catppuccinBat = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "bat";
-    rev = "main";
-    hash = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
   };
 in
 {
@@ -45,65 +38,39 @@ in
       };
 
       opener = {
-        edit = [
-          {
-            run = "micro \"$@\"";
-            block = true;
-            desc = "Edit in micro";
-          }
-        ];
+        edit = [{
+          run = "micro \"$@\"";
+          block = true;
+          desc = "Edit in micro";
+        }];
 
-        open = [
-          {
-            run = "open \"$@\"";
-            desc = "Open";
-          }
-        ];
+        open = [{
+          run = "open \"$@\"";
+          desc = "Open";
+        }];
 
-        reveal = [
-          {
-            run = "open -R \"$1\"";
-            orphan = true;
-            desc = "Reveal in Finder";
-          }
-        ];
+        reveal = [{
+          run = "open -R \"$1\"";
+          orphan = true;
+          desc = "Reveal in Finder";
+        }];
       };
 
-      open = {
-        rules = [
-          {
-            mime = "text/*";
-            use = [ "edit" ];
-          }
-          {
-            name = "*.md";
-            use = [ "edit" ];
-          }
-        ];
-      };
-    };
-
-    plugins = {
-      # Adds full borders around panes
-      "full-border" = pkgs.yaziPlugins."full-border";
-
-      # Shows Git status in Yazi
-      git = pkgs.yaziPlugins.git;
-
-      # Archive integration
-      ouch = pkgs.yaziPlugins.ouch;
-
-      # Interactive chmod inside Yazi
-      chmod = pkgs.yaziPlugins.chmod;
-
-      # macOS Finder tags integration
-      mactag = pkgs.yaziPlugins.mactag;
+      open.rules = [
+        { mime = "text/*"; use = [ "edit" ]; }
+        { name = "*.md";   use = [ "edit" ]; }
+      ];
     };
   };
 
-  xdg.configFile."yazi/theme.toml".text =
-    builtins.readFile "${catppuccinYazi}/themes/mocha/catppuccin-mocha-mauve.toml";
+  xdg.configFile."yazi/theme.toml".text = ''
+    [flavor]
+    dark = "catppuccin-mocha"
+  '';
 
-  xdg.configFile."yazi/Catppuccin-mocha.tmTheme".text =
-    builtins.readFile "${catppuccinBat}/themes/Catppuccin Mocha.tmTheme";
+  xdg.configFile."yazi/flavors/catppuccin-mocha.yazi/flavor.toml".source =
+    "${catppuccinFlavors}/catppuccin-mocha.yazi/flavor.toml";
+
+  xdg.configFile."yazi/flavors/catppuccin-mocha.yazi/tmtheme.xml".source =
+    "${catppuccinFlavors}/catppuccin-mocha.yazi/tmtheme.xml";
 }
