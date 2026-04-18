d3fd2223cfee723bc9f104c476b23e16878647fb
Ven <209770139+venvaneless@users.noreply.github.com>
2026-04-18 12:56:15 +0200

    Trying to apply eza and bat theme 2026-04-18 12:50

--- darwin/modules/terminal/plugins/bat.nix
+++ darwin/modules/terminal/plugins/bat.nix
@@ -1,21 +1,31 @@
-programs.bat = {
-  enable = true;
+# /Users/ven/.config/nix/nix-darwin/darwin/modules/terminal/bat.nix
 
-  themes = {
-    rose-pine-moon = {
-      src = pkgs.fetchFromGitHub {
-        owner = "drluckyspin";
-        repo = "rose-pine-bat";
-        rev = "main";
-        hash = lib.fakeHash;
+{ pkgs, lib, ... }:
+
+{
+  programs.bat = {
+    enable = true;
+
+    themes = {
+      rose-pine-moon = {
+        src = pkgs.fetchFromGitHub {
+          owner = "drluckyspin";
+          repo = "rose-pine-bat";
+          rev = "main";
+          hash = lib.fakeHash;
+        };
+        file = "themes/Rose-Pine-Moon.tmTheme";
       };
-      file = "Rose-Pine-Moon.tmTheme";
+    };
+
+    settings = {
+      theme = "Rose-Pine-Moon";
+      paging = "auto";
+      pager = "less -R";
     };
   };
 
-  settings = {
-    theme = "Rose-Pine-Moon";
-    paging = "auto";
-    pager = "less -R";
+  programs.zsh.shellAliases = {
+    cat = "bat";
   };
-};
+}
