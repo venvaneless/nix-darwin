dee3d90b07194e50a3104fdba9f7563e1c11fab7
Ven <209770139+venvaneless@users.noreply.github.com>
2026-05-13 11:39:21 +0200

    Install Librewolf

---
+++ darwin/modules/apps/librewolf.nix
@@ -0,0 +1,38 @@
+# /Users/ven/.config/nix/nix-darwin/darwin/modules/apps/librewolf.nix
+# 
+# ====================================================================
+# KIWIX
+# A fork of the Firefox web browser
+# ====================================================================
+
+{ ... }:
+
+let
+# App metadata
+# ------------------------------------------------------------
+	appName   = "Librewolf.app";
+	caskName  = "librewolf";
+  targetDir = "/Applications";
+in
+{
+
+# Homebrew cask install
+# ------------------------------------------------------------
+  homebrew.casks = [
+    {
+      name = caskName;
+      args = { appdir = targetDir; };
+    }
+  ];
+
+  # Ensure application directory exists
+  # ------------------------------------------------------------
+  system.activationScripts.ensureLibrewolfAppDir.text = ''
+    if [ ! -d "${targetDir}" ]; then
+      echo "[${appName}] Creating application directory: ${targetDir}"
+      mkdir -p "${targetDir}"
+    else
+      echo "[${appName}] Application directory already exists: ${targetDir}"
+    fi
+  '';
+}
