07096c3bb79fcbf2887ef66f22b8aa296a1eeeac
Ven <209770139+venvaneless@users.noreply.github.com>
2026-06-10 08:21:19 +0200

    Vaultwarden fixed for now 2026-06-10 08:21

--- darwin/modules/services/services.nix
+++ darwin/modules/services/services.nix
@@ -31,7 +31,6 @@
     ./docker/vaultwarden/vaultwarden-service.nix
     ./docker/vaultwarden/vaultwarden-mkcert.nix
     ./docker/vaultwarden/vaultwarden-nginx.nix
-    # ./docker/vaultwarden/vaultwarden-migrate-launchd.nix
     ./docker/vaultwarden/vaultwarden-android-cert.nix
   ];
 }
