fbb791ff5afa40b74d6e56dcd6ecf299e14ec1d8
Ven <209770139+venvaneless@users.noreply.github.com>
2026-06-10 07:48:02 +0200

    Adding fzf trash and iCloud functions

--- darwin/modules/apps/devdocs.nix
+++ darwin/modules/apps/devdocs.nix
@@ -1,5 +1,6 @@
 # /Users/ven/.config/nix/nix-darwin/darwin/modules/apps/devdocs.nix
 #
+# Test
 # =============================================================
 # DevTools
 # API documentation viewer
