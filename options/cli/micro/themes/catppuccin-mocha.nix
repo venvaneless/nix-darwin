fcd8e1365eff474c796344c1f70799aaf1e84205
Ven <209770139+venvaneless@users.noreply.github.com>
2026-09-16 01:25:30 +0200

    micro; naming consistency

--- options/cli/micro/themes/catppuccin-mocha.nix
+++ options/cli/micro/themes/catppuccin-mocha.nix
@@ -10,11 +10,11 @@
 { config, lib, ... }:
 
 let
-  microCfg = config.cli.micro;
+  microCfg = config.home.shared.cli.micro;
   cfg = microCfg.themes.catppuccin;
 in
 {
-  options.cli.micro.themes.catppuccin.enable = lib.mkOption {
+  options.home.shared.cli.micro.themes.catppuccin.enable = lib.mkOption {
     type = lib.types.bool;
     default = false;
     description = "Internal switch for the Catppuccin Mocha theme selected in default.nix.";
